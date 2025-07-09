901d54228b436981cb90a2ed29241c84ac60f611
Bhanukiran Reddy <bkreddy889@gmail.com>
2025-07-09 13:15:12 +0530

    update handle logout

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
   e.preventDefault();
   try {
-    const res = await fetch("http://localhost:5000/api/auth/login", {
+    const res = await fetch("/auth/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       credentials: "include", // important to include cookies!
